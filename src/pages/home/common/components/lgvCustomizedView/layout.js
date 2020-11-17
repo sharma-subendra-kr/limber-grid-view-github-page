d66e57b02c48dfc167f95d96bc967f456b7b7743
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2020-11-17 13:41:23 +0530

    update

--- src/pages/home/common/components/lgvCustomizedView/layout.js
+++ src/pages/home/common/components/lgvCustomizedView/layout.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 
 import { DropDown } from "src/common/components/widgets/dropdown/dropdown";
-import { getRandomTitle } from "src/pages/home/common/utils/utils";
+import { getTitle } from "src/pages/home/common/utils/utils";
 import { Kpi } from "../kpi/kpi";
 import { BarChart } from "../barChart/barChart";
 import { PieChart } from "../pieChart/pieChart";
@@ -12,24 +12,27 @@ const Comps = [Kpi, BarChart, PieChart];
 export const Layout = (props) => {
   const { index, width, height, isAdd, onRemove } = props;
 
-  const [renderBody, setRenderBody] = useState(!isAdd);
-
-  const rand = Math.floor(Math.random() * 2);
-
-  const Comp = Comps[rand];
+  const [renderBody, setRenderBody] = useState(
+    isAdd ? 0 : Math.floor(Math.random() * 2) + 1
+  );
+  const [random] = useState(Math.floor(Math.random() * 11));
 
   const onSelectCb = (key) => {
+    const rand = Math.floor(Math.random() * 2);
+    console.log("rand", rand);
     if (key === "add") {
-      setRenderBody(true);
+      setRenderBody(rand + 1);
     } else {
       onRemove(index);
     }
   };
 
+  const Comp = Comps[renderBody - 1];
+
   return (
     <div className="custom-layout">
       <div className="custom-layout-header">
-        <p className="custom-layout-header-title">{getRandomTitle()}</p>
+        <p className="custom-layout-header-title">{getTitle(random)}</p>
         <div className="custom-layout-header-drop-down">
           <DropDown onSelectCb={onSelectCb} />
         </div>

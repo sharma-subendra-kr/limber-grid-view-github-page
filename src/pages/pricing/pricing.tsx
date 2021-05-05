0cfeb035eb8ac1ef1be3ed6af63bc9fb08256158
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-05-06 01:06:41 +0530

    update

--- src/pages/pricing/pricing.tsx
+++ src/pages/pricing/pricing.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState, useRef } from "react";
 import { connect } from "react-redux";
 
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 import Footer from "src/common/components/static/footer/footer";
 import Header from "src/common/components/static/header/header";
@@ -15,7 +23,10 @@ const Pricing = (props) => {
 			<Drawer />
 			<Footer />
 			<PageContainer>
-				<Paper>Arsenal</Paper>
+				<Typography variant="h4">Pricing</Typography>
+				<br />
+				<br />
+				<Typography variant="h5">Desktop</Typography>
 			</PageContainer>
 		</>
 	);

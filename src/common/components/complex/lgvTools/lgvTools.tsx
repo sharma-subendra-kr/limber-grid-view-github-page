1a7bffe7d59397168e2853b3c8b0ecbeedbf92bb
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-05-08 14:15:20 +0530

    update

--- src/common/components/complex/lgvTools/lgvTools.tsx
+++ src/common/components/complex/lgvTools/lgvTools.tsx
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+
+import AddOutlinedIcon from "@material-ui/icons/AddOutlined";
+import TouchAppOutlinedIcon from "@material-ui/icons/TouchAppOutlined";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faCut,
-	faPlus,
-	faExpandArrowsAlt,
-} from "@fortawesome/free-solid-svg-icons";
-import { faClone, faHandPointUp } from "@fortawesome/free-regular-svg-icons";
+import { faCut } from "@fortawesome/free-solid-svg-icons";
+
+import styled from "styled-components";
 
 import "./lgvTools.scss";
 
 const LgvTools = (props) => {
 	const {
-		onResizeMethodChange,
+		className,
+		// onResizeMethodChange,
 		onLatchChange,
 		onDeskInteractionChange,
 		onAddItem,
 	} = props;
 
-	const [resizeMethod, setResizeMethod] = useState(false);
+	// const [resizeMethod, setResizeMethod] = useState(false);
 	const [latch, setLatch] = useState(true);
-	const [addOrCut, setAddOrCut] = useState("ADD"); // ADD or CUTSPACE
+	const [addOrCut, setAddOrCut] = useState("CUTSPACE"); // ADD or CUTSPACE
 
-	const onResizeMethodClick = (event) => {
-		setResizeMethod(!resizeMethod);
+	// const onResizeMethodClick = (event) => {
+	// 	setResizeMethod(!resizeMethod);
 
-		if (onResizeMethodChange) {
-			onResizeMethodChange(!resizeMethod);
-		}
-	};
+	// 	if (onResizeMethodChange) {
+	// 		onResizeMethodChange(!resizeMethod);
+	// 	}
+	// };
 
 	const onLatchClick = (event) => {
 		setLatch(!latch);
@@ -60,44 +68,74 @@ const LgvTools = (props) => {
 	};
 
 	return (
-		<div className="lgv-tools">
-			<div className="lgv-tool lgv-tool-latch">
-				<input
-					type="checkbox"
-					value={resizeMethod}
-					defaultChecked={resizeMethod}
-					onClick={onResizeMethodClick}
+		<div className={`${className} lgv-tools`}>
+			<FormGroup row>
+				<FormControlLabel
+					control={
+						<Checkbox size="small" checked={latch} onClick={onLatchClick} />
+					}
+					label="Latch on move/resize"
 				/>
-				<FontAwesomeIcon icon={faExpandArrowsAlt} rotation={90} />
-				Arrange Vertical on Resize
-			</div>
-			<div className="lgv-tool lgv-tool-latch">
-				<input
-					type="checkbox"
-					value={latch}
-					defaultChecked={latch}
-					onClick={onLatchClick}
+				<div className="seperator" />
+				<RadioGroup
+					name="addOrCut"
+					row
+					onChange={onAddOrCutClick}
+					value={addOrCut}
+				>
+					<FormControlLabel
+						value="CUTSPACE"
+						control={<Radio size="small" />}
+						label={
+							<>
+								<FontAwesomeIcon icon={faCut} />
+								{"  "}
+								Cut Space
+							</>
+						}
+					/>
+					<FormControlLabel
+						value="ADD"
+						control={<Radio size="small" />}
+						label={
+							<>
+								<TouchAppOutlinedIcon />
+								{"  "}Add item by dragging
+							</>
+						}
+					/>
+				</RadioGroup>
+				<div className="seperator" />
+				<FormControlLabel
+					control={
+						<IconButton size="small" onClick={onAddItemClick}>
+							<AddOutlinedIcon />
+						</IconButton>
+					}
+					label="Add item"
 				/>
-				<FontAwesomeIcon icon={faClone} rotation={90} />
-				Latch on move / resize
-			</div>
-			{addOrCut === "ADD" ? (
-				<div className="lgv-tool" onClick={onAddOrCutClick}>
-					<FontAwesomeIcon icon={faCut} />
-					Cut Space
-				</div>
-			) : (
-				<div className="lgv-tool" onClick={onAddOrCutClick}>
-					<FontAwesomeIcon icon={faHandPointUp} />
-					Add by dragging
-				</div>
-			)}
-			<div className="lgv-tool" onClick={onAddItemClick}>
-				<FontAwesomeIcon icon={faPlus} />
-				Add Item
-			</div>
+			</FormGroup>
 		</div>
 	);
 };
 
-export default LgvTools;
+const StyledLgvTools = styled(LgvTools)`
+	span {
+		font-size: 12px !important;
+	}
+	.seperator {
+		width: 2px;
+		height: 100%;
+		margin: 0 10px;
+		background-color: #c0c0c0;
+	}
+	.MuiButtonBase-root {
+		padding-top: 0;
+		padding-bottom: 0;
+	}
+	.MuiSvgIcon-root {
+		vertical-align: middle;
+	}
+`;
+
+export default StyledLgvTools;

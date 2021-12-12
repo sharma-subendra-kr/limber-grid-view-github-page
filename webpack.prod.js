cd43675bf21860e6d618c76175771892a846e64a
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-12-12 18:10:00 +0530

    WIP: Issue#11 - externalizing third party libraries

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,47 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
+function externalForMaterialUi({ context, request }, callback) {
+	if (/@material-ui.+/.test(request)) {
+		const name = request.replace(/^.*[\\\/]/, "");
+		return callback(null, "root MaterialUI." + name);
+	}
+	callback();
+}
+
 module.exports = merge(common, {
 	mode: "production",
 	output: {
 		path: __dirname + "/dist",
-		filename:	"index.js",
+		filename: "index.js",
 	},
-	externals: {
-		"resize-observer-polyfill": {
-			commonjs: "resize-observer-polyfill",
-			commonjs2: "resize-observer-polyfill",
-			amd: "resize-observer-polyfill",
-			root: "resize-observer-polyfill",
+	externals: [
+		// "@fortawesome",
+		"/@material-ui/core/.*/",
+		// "/@material-ui/icons/.*/",
+		// "/@material-ui/lab/.*/",
+		// externalForMaterialUi,
+		{ "@reduxjs/toolkit": "RTK" },
+		{ "connected-react-router": "ConnectedReactRouter" },
+		{
+			lodash: {
+				commonjs: "lodash",
+				commonjs2: "lodash",
+				amd: "lodash",
+				root: "_",
+			},
 		},
-	},
+		{ "markdown-to-jsx": "MarkdownToJSX" },
+		{ react: "React" },
+		{ "react-is": "ReactIs" },
+		{ "react-dom": "ReactDOM" },
+		{ "react-redux": "ReactRedux" },
+		{ "react-router-dom": "ReactRouterDOM" },
+		// { recharts: "Recharts" },
+		{ redux: "Redux" },
+		{ "redux-saga": "ReduxSaga" },
+		// Sanitize html should be removed from front end
+		// { "sanitize-html": "sanitizeHtml" },
+		{ "styled-components": "styled" },
+	],
 });

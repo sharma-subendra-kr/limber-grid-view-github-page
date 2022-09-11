728371e8cd555063c744d040e2f8a834eb2176da
Subendra Sharma <32413952+sharma-subendra-kr@users.noreply.github.com>
2022-09-11 18:55:59 +0530

    update

--- src/pages/development/ducks.ts
+++ src/pages/development/ducks.ts
@@ -9,9 +9,14 @@ const CHANGE_RESIZE_METHOD_ACTION = "[development] change resize method action";
 const CHANGE_RESIZE_METHOD = "[development] change resize method";
 const CHANGE_DESK_INTERACTION_MODE_ACTION =
 	"[development] change desk interaction mode action";
-const CHANGE_DESK_INTERACTION_MODE = "[development] change desk interaction mode";
+const CHANGE_DESK_INTERACTION_MODE =
+	"[development] change desk interaction mode";
 const SET_POSITION_DATA_ACTION = "[development] set position data action";
 const SET_POSITION_DATA = "[development] set position data";
+const SET_MARGIN_ACTION = "[home] set margin action";
+const SET_MARGIN = "[home] set margin";
+const SET_MARGIN_CHANGE_VALUE_ACTION = "[home] set margin change value action";
+const SET_MARGIN_CHANGE_VALUE = "[home] set margin change value";
 
 const initialState = {
 	view: "customized",
@@ -19,6 +24,8 @@ const initialState = {
 	resizeMethod: false,
 	deskInteractionMode: "CUTSPACE",
 	positionData: undefined,
+	margin: undefined,
+	marginChangeValue: undefined,
 };
 
 export const stateSelector = (state) => state.developmentReducer;
@@ -43,6 +50,14 @@ export const getPositionData = createSelector(stateSelector, (state) => {
 	return state.positionData;
 });
 
+export const getMargin = (state) => {
+	return stateSelector(state).margin;
+};
+
+export const getMarginChangeValue = (state) => {
+	return stateSelector(state).marginChangeValue;
+};
+
 export const developmentReducer = createReducer(initialState, {
 	[CHANGE_VIEW]: (state, { payload }) => {
 		state.view = payload;
@@ -59,6 +74,12 @@ export const developmentReducer = createReducer(initialState, {
 	[SET_POSITION_DATA]: (state, { payload }) => {
 		state.positionData = payload;
 	},
+	[SET_MARGIN]: (state, { payload }) => {
+		state.margin = payload;
+	},
+	[SET_MARGIN_CHANGE_VALUE]: (state, { payload }) => {
+		state.marginChangeValue = payload;
+	},
 });
 
 export const changeViewAction = createAction(CHANGE_VIEW_ACTION);
@@ -70,6 +91,10 @@ export const changeDeskInteractionModeAction = createAction(
 	CHANGE_DESK_INTERACTION_MODE_ACTION
 );
 export const setPositionDataAction = createAction(SET_POSITION_DATA_ACTION);
+export const setMarginAction = createAction(SET_MARGIN_ACTION);
+export const setMarginChangeValueAction = createAction(
+	SET_MARGIN_CHANGE_VALUE_ACTION
+);
 
 function* changeViewSaga({ payload }) {
 	yield put({ type: CHANGE_VIEW, payload: payload });
@@ -91,6 +116,14 @@ function* setPositionDataSaga({ payload }) {
 	yield put({ type: SET_POSITION_DATA, payload: payload });
 }
 
+function* setMarginSaga({ payload }) {
+	yield put({ type: SET_MARGIN, payload });
+}
+
+function* setMarginChangeValueSaga({ payload }) {
+	yield put({ type: SET_MARGIN_CHANGE_VALUE, payload });
+}
+
 export function* developmentSaga() {
 	yield all([
 		yield takeLatest(CHANGE_VIEW_ACTION, changeViewSaga),
@@ -101,5 +134,7 @@ export function* developmentSaga() {
 			changeDeskInteractionModeSaga
 		),
 		yield takeLatest(SET_POSITION_DATA_ACTION, setPositionDataSaga),
+		yield takeLatest(SET_MARGIN_ACTION, setMarginSaga),
+		yield takeLatest(SET_MARGIN_CHANGE_VALUE_ACTION, setMarginChangeValueSaga),
 	]);
 }
